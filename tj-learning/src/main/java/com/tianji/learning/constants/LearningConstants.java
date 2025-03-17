8e3e3c0dd69fca664088d53503e3e8a6931bd408
Lusy37 <luziqiang333@gmail.com>
2025-03-17 09:11:02 +0800

    开发签到和积分排行榜，使用 MyBatisPlus 分表优化

---
+++ tj-learning/src/main/java/com/tianji/learning/constants/LearningConstants.java
@@ -0,0 +1,8 @@
+package com.tianji.learning.constants;
+
+public interface LearningConstants {
+
+    String POINTS_BOARD_KEY_PREFIX = "points_board_";
+
+    String POINTS_RECORD_TABLE_PREFIX = "points_record_";
+}
